8d6e674ca68122bdc384374d00bb7a4dfc319783
michael1233 <emmamichael201@gmail.com>
2024-12-17 14:25:50 +0100

    fix code challenge

--- module2/code.py
+++ module2/code.py
@@ -12,32 +12,44 @@ def convertHexDec(value):
         "C": 12,
         "D": 13,
         "E": 14,
-        "F": 15
+        "F": 15,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+         "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "0": 0,
     }
     for char in newValue[0]:
         # Convert hex character to decimal if it's a letter
         if char in alphabetsHex:
             char = alphabetsHex[char]
         else:
-            char = int(char)  # Convert numeric characters to integers
-
+            return None
         # Update result using the hexadecimal to decimal formula
         result += char * (16 ** length)
         length -= 1  # Decrement the power of 16
-    print(result)
     if len(newValue) == 2 : 
          for char2 in newValue[1] :
             if char2 in alphabetsHex :
                 char2= alphabetsHex[char2]
             else :
-                char2= int(char2)
+                return None
             result += char2 * (16 ** negLength)
             negLength -= 1
+            
     return result
 
 # Test the function
-ans = convertHexDec("B3F.A1")
-ans2= convertHexDec("3A.2F")
+ans = convertHexDec("C")
+# ans2= convertHexDec("3A.2F")
 
 print(ans)
-print(ans2)
+# print(ans2)
+
+print(int(8.7))
+print(bool(-1) == 1)
